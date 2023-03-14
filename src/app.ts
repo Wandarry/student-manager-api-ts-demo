8d92cca72e1771a5ac1691fe7c6763d338081666
Wandarry <berryenick20@gmail.com>
2023-03-14 23:06:12 +0100

    crud methods

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,60 @@
 import express, {Request, Response} from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.json());
 
-app.get('/data/:studentId',(req:Request, res:Response) =>{
-    res.send(req.params);
+async function newStudents() {
+    const students = await prisma.student.createMany({
+        data: [
+            {
+                name: "student1",
+                email: "first@gmail.com"
+            },
+
+            {
+                name: "enick",
+                email: "enick@gmail.com"
+            },
+
+            {
+                name: "student8",
+                email: "eighth@gmail.com"
+            },
+        ]
+    })
+
+}
+
+app.get('/', async (req: Request, res: Response) => {
+    const newStudent = await newStudents();
+    const students = await prisma.student.findMany();
+    res.send(students);
 })
 
+app.post('/firstStudent', async(req: Request, res: Response) => {
+    const firstStudent = await prisma.student.findUnique({
+        where: {
+            id: 1
+        }
+    })
+    res.send(firstStudent)
+})
 
-app.listen(3000, () => {
-    console.log("App listening at http://localhost:3000");
+app.delete('/', async(req: Request, res: Response) => {
+    const deleteStudent = await prisma.student.delete({
+        where: {
+            email: "blabla@gmail.com"
+        },
+    });
+
+    const listOfStudents = await prisma.student.findMany();
+    res.send(listOfStudents);
+})
+
+app.listen(port, () => {
+    console.log("Listening at port ", port)
 })
